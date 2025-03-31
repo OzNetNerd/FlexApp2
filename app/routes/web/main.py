e3b3b856b7831a935d96f48dbfc0fdda0ad86a52
Will Robinson <will@oznetnerd.com>
2025-03-31 12:16:54 +1100

    update

--- app/routes/web/main.py
+++ app/routes/web/main.py
@@ -1,7 +1,7 @@
 import logging
 from app.routes.web import main_bp
 from app.routes.base.components.template_renderer import render_safely
-from app.routes.base.components.form_handler import ResourceContext
+from app.routes.base.components.form_handler import BasicContext
 
 logger = logging.getLogger(__name__)
 
@@ -9,6 +9,6 @@ logger = logging.getLogger(__name__)
 @main_bp.route("/")
 def index():
     logger.debug("Serving main index page")
-    context = ResourceContext()
+    context = BasicContext(title="Index")
     fallback_message = "Sorry, we couldn't load the homepage. Please try again later."
     return render_safely("index.html", context, fallback_message)
