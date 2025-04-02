3ff8d7d8e715b1aaaf8c8d7b47131a325b9e9a2a
Will Robinson <will@oznetnerd.com>
2025-04-02 15:25:11 +1100

    update

---
+++ app/routes/base/tabs/__init__.py
@@ -0,0 +1,9 @@
+from app.routes.base.tabs.contacts import contacts_tabs
+
+# In your tabs module or a dedicated registry file
+UI_TAB_MAPPING = {
+    'Contact': contacts_tabs,
+    # 'Company': companies_tabs,
+}
+
+
