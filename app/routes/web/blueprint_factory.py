72839f4c5d55eeea3f05107fb64d40d0676e72ad
root <root@Will-PC.>
2025-04-24 11:46:31 +1000

    refactor html

--- app/routes/web/blueprint_factory.py
+++ app/routes/web/blueprint_factory.py
@@ -1,30 +1,30 @@
 # app/routes/web/blueprint_factory.py
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import login_required
+from flask import Blueprint
 
-from app.routes.web.context import EntityContext, TableContext
-from app.routes.web.context_utils import use_context
-from app.routes.web.route_registration import default_crud_templates
+from app.routes.web.route_registration import (
+    default_crud_templates,
+    register_crud_routes,
+    CrudRouteConfig
+)
 from app.services.crud_service import CRUDService
 from app.utils.app_logging import get_logger
 
 logger = get_logger()
 
 
-# Simplified version of create_crud_blueprint
 def create_crud_blueprint(model_class):
     blueprint_name = f"{model_class.__tablename__}_bp"
     blueprint = Blueprint(blueprint_name, __name__, url_prefix=f"/{model_class.__tablename__}")
+
     service = CRUDService(model_class)
     templates = default_crud_templates(model_class.__entity_name__)
+    config = CrudRouteConfig(
+        blueprint=blueprint,
+        entity_table_name=model_class.__entity_name__,
+        service=service,
+        templates=templates
+    )
 
-    @blueprint.route("/", methods=["GET"])
-    @use_context(TableContext, model_class=model_class, action="index")
-    @login_required
-    def index(context):
-        return render_template(templates.index, **context.to_dict())
-
-    # Other routes similar to your existing code...
-
+    register_crud_routes(config)
     return blueprint
