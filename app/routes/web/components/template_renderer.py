e5b9fd47f224afa44c7bb0cad15dc4419adbec83
root <root@Will-PC.>
2025-05-02 00:04:55 +1000

    working

--- app/routes/web/components/template_renderer.py
+++ app/routes/web/components/template_renderer.py
@@ -4,14 +4,14 @@ import inspect
 import json
 import traceback
 from dataclasses import dataclass
-from typing import Optional, Tuple, Union
+from typing import Tuple, Union
 
 from flask import abort, current_app, get_flashed_messages, render_template, request, url_for
 from jinja2 import DebugUndefined, Environment
 from jinja2.exceptions import TemplateNotFound
 from markupsafe import Markup, escape
 
-from app.routes.web.context import WebContext
+from app.routes.web.context import BaseContext
 from app.utils.app_logging import get_logger, log_message_and_variables
 
 logger = get_logger()
@@ -20,7 +20,7 @@ logger = get_logger()
 @dataclass
 class RenderSafelyConfig:
     template_path: str
-    context: WebContext
+    context: BaseContext
     error_message: str
     endpoint_name: str
 
--- app/routes/web/home.py
+++ app/routes/web/home.py
@@ -3,8 +3,10 @@
 from flask import Blueprint
 from flask_login import current_user, login_required
 
+
 from app.routes.web.components.template_renderer import render_safely, RenderSafelyConfig
 from app.utils.app_logging import get_logger
+from app.routes.web.context import WebContext
 
 logger = get_logger()
 home_bp = Blueprint("home_bp", __name__, url_prefix="/")
@@ -20,8 +22,8 @@ def index():
     config = RenderSafelyConfig(
         template_path="pages/misc/home.html",
         error_message="Error rendering dashboard home page",
-        endpoint_name="index"
-        con
+        endpoint_name="index",
+        context=WebContext(title="Home Page"),
     )
 
     return render_safely(config)
