bccca29f02ff6ccee14a87492ef95645b51cb1a8
root <root@Will-PC.>
2025-05-05 13:23:31 +1000

    ddd migration

---
+++ app/graphql/__init__.py
@@ -0,0 +1,9 @@
+# app/graphql/__init__.py
+from strawberry.flask.views import GraphQLView
+from app.graphql.schema import schema
+
+def init_graphql(app):
+    app.add_url_rule(
+        '/graphql',
+        view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+    )
